166a23df4b64a0d025c9e72ed903c2e66eb870af
chancmg <chancmg007@gmail.com>
2019-01-26 17:36:43 +0530

    project revamping

--- app/containers/dashboard.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import Dashboard from '../components/dashboard';
-
-const DashboardPage = () => <Dashboard />;
-export default DashboardPage;
--- app/containers/home.js
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-
-const Home = () => <h1>Testing component</h1>;
-export default Home;
